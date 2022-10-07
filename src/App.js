84486c9f988f9da299d67920df2bd7f3930a2f27
Ashley <ashleynjeri7@gmail.com>
2022-10-07 12:05:50 +0300

    new component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React from "react";
 import {Routes, Route} from 'react-router-dom'
 import NavBar from "./NavBar";
 import Home from "./Home";
+import Guide from "./Guide";
 import Quiz from "./Quiz";
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Routes> 
           <Route exact='true' path='/' element={<Home />} ></Route>
           <Route exact='true' path='/home' element={<Home />} ></Route>
+          <Route exact='true' path='/guide' element={ <Guide />} ></Route>
           <Route exact='true' path='/quiz' element={ <Quiz />} ></Route>
       </Routes>
     </div>
---
+++ src/Guide.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Guide = () => {
+    return (
+        <div>
+            <h2>Hello! Welcome to this quiz</h2>
+        </div>
+    )
+}
+export default Guide;
