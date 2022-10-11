16138e7ce2bcc7d4407ff9a220d8e8a310d8b26e
Ashley <ashleynjeri7@gmail.com>
2022-10-11 13:35:13 +0300

    Updates

---
+++ precache-manifest.5f9fea27bf109bdb21a2ded868938894.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f13e2cd1fc239d1735e3",
+    "url": "/trivia-app/static/css/main.1896417a.chunk.css"
+  },
+  {
+    "revision": "f13e2cd1fc239d1735e3",
+    "url": "/trivia-app/static/js/main.f13e2cd1.chunk.js"
+  },
+  {
+    "revision": "b51f3b9ecbc29aa46dac",
+    "url": "/trivia-app/static/js/1.b51f3b9e.chunk.js"
+  },
+  {
+    "revision": "a66daceb0e627306f47c",
+    "url": "/trivia-app/static/js/runtime~main.a66daceb.js"
+  },
+  {
+    "revision": "14bee8722f4ca249bb8ecf8412c95903",
+    "url": "/trivia-app/index.html"
+  }
+];
